aacff8cfe0ed85fd381d81e19845da04e32d1943
Fra001 <50527750+Fra001@users.noreply.github.com>
2025-07-22 16:44:12 +0200

    Create App.js
    cd ..
    npx create-react-app client

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Easy BTC Store</h1>
+      <p>Calcola BTC ricevuto da più exchange.</p>
+    </div>
+  );
+}
+
+export default App;
